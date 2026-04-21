6f33d727899b3f18903bc239a21943e877c7ea85
asgersvenning <asgersvenning@gmail.com>
2026-04-21 15:28:08 +0200

    Fix extraneous print statement

--- .github/scripts/validate_datasets.py
+++ .github/scripts/validate_datasets.py
@@ -241,7 +241,6 @@ def main():
     if args.format == "json":
         print(json.dumps(checks))
     else:
-        print(checks)
         for ds, rule_results in checks.items():
             print(f"\nDataset: {ds}")
             image = rule_results["image"]
